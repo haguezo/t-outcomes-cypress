983937ce8fbcca8a48edf527e5942ceac4d88e08
zoe.hague@outlook.com <zoe.hague@outlook.com>
2021-07-21 09:11:39 +0100

    Added tests to check links

--- cypress/integration/provider_spec.js
+++ cypress/integration/provider_spec.js
@@ -3,33 +3,26 @@ describe('Navigating to a provider page', () => {
     // EXAMPLE: Using 'require' and a const for test data from a fixture
     const nations = require('../fixtures/nations.json')
 
-    afterEach(() => {
-        cy.reload()
-    })
-
-    // EXAMPLE: Nested tests
     nations.list.forEach((nation) => {
 
-        it.only(`Clicking ${nation} displays expected count of providers `, () => {
-            // act
-            cy.getByTestId(`${nation}Button`).click()
+        nations[nation.toLowerCase()].providers.forEach((provider) => {
+            it.only(`Link for '${provider.name}' in ${nation} is displayed with correct URL for UKPRN ${provider.ukprn} and request for href returns 200`, () => {
+    
+                // act
+                cy.getByTestId(`${nation}Button`).click()
 
-            nations[nation.toLowerCase()].providers.forEach((provider) => {
-                it(`Link for '${provider.name}' is displayed with correct URL containing UKPRN ${provider.ukprn} and navigates to provider page`, () => {
+                // EXAMPLE: Testing a link including making an HTTP request to ensure it is valid and returning expected status 
+                cy.get(`a:contains(${provider.name})`)
+                    .should('be.visible')
+                    .invoke('attr', 'href')
+                    .should('eq', `#/tefoutcomes/provider/${provider.ukprn}`)
+                    .then(href => {
+                        cy.request(href).its('status').should('eq', 200)
+                    })
 
-                    // Check a provider link is found with the name
-                    cy.getByTestId('providerLink')
-
-                    // Check the link href is correct
-
-                    // Click the link
-
-                    // Check new url
-                })
-            })
-
-            // assert
-            cy.getByTestId(`${nation}Badge`).contains(nations.totals[`${nation}`])
+                cy.reload()
+            })       
         })
+
     })
 })
